1d04d24b9d614a01941b9040d5292d4e508ad170
Adamkif <adam.sirkif@gmail.com>
2020-06-24 22:54:18 +0000

    responsive

---
+++ lib/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'content.dart';
+import 'menu.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(context) => Scaffold(
+        appBar: AppBar(title: Text("test")),
+        drawer: MediaQuery.of(context).size.width < 500
+            ? Drawer(
+                child: Menu(),
+              )
+            : null,
+        body: SafeArea(
+          child: Center(
+            child: MediaQuery.of(context).size.width < 500
+                ? Content()
+                : Row(
+                    children: [
+                      Container(
+                        // This width for drawer
+                        width: 200.0,
+                        child: Menu(),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width - 200.0,
+                        child: Content(),
+                      )
+                    ],
+                  ),
+          ),
+        ),
+      );
+}
